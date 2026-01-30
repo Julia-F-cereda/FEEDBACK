3915806d8d2e9d7dd9f58b05d4c8edd8811167c3
Lorena Gonçalves <helena.rosa@aluno.senai.br>
2026-01-30 17:16:16 -0300

    COMITY: fim da aula

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 #importando 
 app = Flask(__name__)
 
@@ -10,7 +10,21 @@ def pg_principal():
 
 @app.route("/sobre")
 def pg_desenvolvedor():
-    return render_template("sobre.html")
+    return render_template("sobre.html", methods=["GET"])
+
+@app.route("/login", methods=["GET"])
+def pg_login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def pg_login_post():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+
+    if usuario == "Julia" and senha == "Juliaabc":
+        return "voce acessou a pagina com sucesso"
+    else:
+        return render_template("/login.html", erro = "Acesso Negado!")
 
 #rodando 
 app.run(debug= True)
